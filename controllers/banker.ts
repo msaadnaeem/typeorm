14c18da321750bd145a2a793b15130727ea04734
msaadnaeem <110536997+msaadnaeem@users.noreply.github.com>
2022-11-12 03:06:19 +0500

    Update banker.ts

--- controllers/banker.ts
+++ controllers/banker.ts
@@ -22,12 +22,8 @@ export const connect = async (req:Request,res:Response)=>{
         const client= await AppDataSource.getRepository(Client).findOneBy({id:client_id})
         const banker= await AppDataSource.getRepository(Banker).findOneBy({id:banker_id})
         if (banker && client) {
-			console.log(banker.clients)
-			banker.first_name='mahnoor'
-			banker.clients = [...banker.clients,client]
+			banker.clients = [client];
 			await banker.save();
-			console.log(banker.clients)
-			
 
 			return res.json({
 				msg: 'banker connected to client',
